793a965b21b74118dad0cfc6b73d77c9d31531dd
allan.klein <allan.klein@fr.gt.com>
2022-09-21 17:00:50 +0200

    a

--- src/main/java/com/example/demo/WebSocketHandler.java
+++ src/main/java/com/example/demo/WebSocketHandler.java
@@ -5,18 +5,63 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class WebSocketHandler extends AbstractWebSocketHandler {
-    // AbstractWebSocketHandler vous oblige à mettre en œuvre deux méthodes, handleTextMessage et handleBinaryMessagequi sont appelés lorsqu'un nouveau message texte ou binaire est reçu.
+    // AbstractWebSocketHandler vous oblige à mettre en œuvre deux méthodes,
+    // handleTextMessage et handleBinaryMessagequi sont appelés lorsqu'un nouveau
+    // message texte ou binaire est reçu.
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
-        System.out.println("New Text Message Received");
+        // System.out.println("New Text Message Received");
         System.out.println(message.getPayload());
         // sendMessage => Envoie un message via WS
-        if(message.getPayload() == "test") {
+        if (message.getPayload().equalsIgnoreCase("test")) {
             session.sendMessage(message);
+            byte[] fileToArrayOfBytes = Files.readAllBytes(Paths.get("./testMoi.json"));
+            System.out.println("Ws1Application.java");
+            int fileSize = fileToArrayOfBytes.length;
+            // int nbrOfArr = fileSize / 7000;
+
+            List<byte[]> multiArr = new ArrayList<byte[]>();
+
+            // int index = 0;
+            int nextIndex = 7000;
+            Gson gson = new Gson();
+            for (int i = 0; i < fileSize; i += nextIndex) {
+                if (i == 7000) {
+                    nextIndex = fileSize - i;
+                }
+                System.out.println(i);
+                byte[] a = Arrays.copyOfRange(fileToArrayOfBytes, i, i + nextIndex);
+                multiArr.add(a);
+
+                // String s = new String(multiArr);
+                String json = gson.toJson(multiArr);
+                session.sendMessage(new TextMessage(json));
+            }
+
+            // System.out.println(fileToArrayOfBytes);
+            // System.out.println(fileToArrayOfBytes.length);
+            // Path currentRelativePath = Paths.get("");
+            // String s = currentRelativePath.toAbsolutePath().toString();
+            // System.out.println("Current absolute path is: " + s);
+
+            // String s = new String(fileToArrayOfBytes);
+            // // System.out.println(s);
+            // Gson gson = new Gson();
+            // String json = gson.toJson(s);
+            // System.out.println(json);
+            // System.out.println("pouet");
         }
     }
 
